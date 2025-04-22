535965874e742a40aa6978da21071d95f6546436
rednaldeirf <jason.d.friedlander@gmail.com>
2025-04-22 10:11:39 -0400

    profile model

---
+++ models/profile.js
@@ -0,0 +1,41 @@
+import mongoose, {Schema} from "mongoose";
+
+const profileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+        // required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'], 
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 0, 
+        required: true
+    },
+    weight: {
+        type: Number, 
+        min: 0,
+        required: true
+    },
+    height: {
+        type: Number, 
+        min: 0,
+        required: true
+    },
+    experienceLevel: {
+        type: String,
+        enum: ['Beginner', 'Intermediate', 'Advanced'], 
+        required: true
+    },
+});
+
+export default mongoose.model("profile", profileSchema);
