efb29761bca78d55989cf407e51f7c048e0a37ed
Albert <albertoma24@gmail.com>
2025-04-22 09:40:57 -0700

    workout get routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,6 +1,29 @@
 import {Router} from 'express';
+import Workout from "../models/workout.js";
 // import * as workoutController from '../controllers/workouts';
 
-const router = Router();
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try{
+        const workouts =await Workout.find().populate("userId","username");
+        res.status(200).json(workouts);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch workouts", error: err});
+    }
+});
+
+router.get("/:workoutId", async (req,res) => {
+    const { workoutId } = req.params;
+    try{
+        const workout = await Workout.findById(workoutId).populate("userId", "username");
+        if (!workout) {
+            return res.status(404).json({message: "Workout not found"});
+        }
+        res.status(200).json(workout);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch workout", error: err });
+    }
+});
 
 export default router;
