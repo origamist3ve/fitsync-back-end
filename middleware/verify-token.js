7a6ca2ff49abc7b62d52423565003e1ccc5a3475
rednaldeirf <jason.d.friedlander@gmail.com>
2025-04-23 13:48:40 -0400

    changes to token

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,21 +1,21 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const verifyToken = (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-        if (!token) {
-            throw new Error("Unauthorized");
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = decoded.payload;
-
-        next();
-    } catch (error) {
-        res.status(401).json({ err: error.message });
+    if (!token) {
+      throw new Error("Unauthorized: No token provided");
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.payload;
+    next();
+  } catch (error) {
+    res.status(401).json({ err: error.message });
+  }
 };
 
 export default verifyToken;
